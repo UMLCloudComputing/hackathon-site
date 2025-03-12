6be270096c7be155b606188b4821794a3e4538dd
Gurpranked <gurpreet.singh.rehal@outlook.com>
2025-03-11 21:05:58 -0400

    chore: Remove scrollbar on club carousel

--- src/components/ClubCarousel.tsx
+++ src/components/ClubCarousel.tsx
@@ -3,7 +3,7 @@ import "../app/globals.css"
 
 export const ClubCarousel = () => {
     return (
-        <div className="flex flex-row gap-10 overflow-x-scroll m-4">
+        <div className="flex flex-row gap-10 overflow-x-scroll m-4 remove_scrollbar">
             <ClubCarouselCard name={"Cloud Computing Club"} />
             <ClubCarouselCard name={"ACM"} />
             <ClubCarouselCard name={"Open Source Club"} />
--- src/components/ClubCarouselCard.tsx
+++ src/components/ClubCarouselCard.tsx
@@ -1,10 +1,10 @@
 import { JBMono } from "@/fonts"
 import Image from "next/image"
-export const ClubCarouselCard = ({ name }: { name: string }) => {
+export const ClubCarouselCard = ({ name, url }: { name: string, url?: string }) => {
     return (
         <div className="bg-red-600 flex-none flex flex-col items-center p-4 gap-6 justify-center text-wrap basis-60 grow-1 shadow-xl rounded-2xl shadow-amber-600">
             <h2 className={`${JBMono.className} text-2xl text-center`}>{name}</h2>
-            <Image alt="test" width={75} height={75} src={"/globe.svg"} className={""} />
+            {url ? <Image alt="test" width={75} height={75} src={url} className={""} /> : <Image alt="test" width={75} height={75} src={"/globe.svg"} className={""} />}
         </div>
     )
 }
