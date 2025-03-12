3e0b5ee591468c3195f7b548b207120c7f1b54ed
Gurpranked <gurpreet.singh.rehal@outlook.com>
2025-03-11 21:16:45 -0400

    chore: Add Cloud Computing Club Logo to Club Carousel

--- src/components/ClubCarousel.tsx
+++ src/components/ClubCarousel.tsx
@@ -4,11 +4,11 @@ import "../app/globals.css"
 export const ClubCarousel = () => {
     return (
         <div className="flex flex-row gap-10 overflow-x-scroll m-4 remove_scrollbar">
-            <ClubCarouselCard name={"Cloud Computing Club"} />
-            <ClubCarouselCard name={"ACM"} />
-            <ClubCarouselCard name={"Open Source Club"} />
-            <ClubCarouselCard name={"Cybersecurity Club"} />
-            <ClubCarouselCard name={"Game Dev Club"} />
+            <ClubCarouselCard name={"Cloud Computing Club"} url={"https://umlcloudcomputing.org/img/club-logo.png"} />
+            <ClubCarouselCard name={"ACM"} url={""} />
+            <ClubCarouselCard name={"Open Source Club"} url={""} />
+            <ClubCarouselCard name={"Cybersecurity Club"} url={""} />
+            <ClubCarouselCard name={"Game Dev Club"} url={""} />
         </div>
     )
 }
