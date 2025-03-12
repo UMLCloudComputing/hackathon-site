6946519e44621cddebc0a838702fbb31b0f87141
Gurpranked <gurpreet.singh.rehal@outlook.com>
2025-03-11 20:28:56 -0400

    chore: Corrected navbar register button

--- src/components/NavBarRegisterButton.tsx
+++ src/components/NavBarRegisterButton.tsx
@@ -2,6 +2,6 @@ import { JBMono } from "@/fonts"
 
 export const NavBarRegisterButton = () => {
     return (
-        <button className={`${JBMono.className} text-3xl bg-[#5bbab5] hover:bg-[#890095] hover-ease-in ease-out duration-100 cursor-pointer border-2 p-1 text-white border-black`}>Register!</button>
+        <button className={`${JBMono.className} text-3xl bg-[#5bbab5] hover:bg-[#890095] hover-ease-in ease-out duration-100 cursor-pointer border-2 p-1 text-white border-black register_button`}>Register!</button>
     )
 }
