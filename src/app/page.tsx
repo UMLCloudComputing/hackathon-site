f86e13858d26c4d64860eb9c4cc74723cd00ae84
Gurpranked <gurpreet.singh.rehal@outlook.com>
2025-03-15 00:06:59 -0400

    chore: Correct text color to be black on landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,15 +6,15 @@ import { InstrSans, JBMono } from "@/fonts";
 export default function Home() {
   return (
     <div className={"bg-[#ffe7ff] flex flex-col jusitfy-center items-center gap-8 h-full"}>
-      <h2 className={`${JBMono.className} text-2xl m-6`}>UML CS Club Coalition Presents...</h2>
-      <h1 className={`${InstrSans.className} font-bold text-8xl`}>RiverHack 2025</h1>
+      <h2 className={`${JBMono.className} text-2xl m-6 text-black`}>UML CS Club Coalition Presents...</h2>
+      <h1 className={`${InstrSans.className} font-bold text-8xl text-black`}>RiverHack 2025</h1>
       <LandingPageRegistrationButton />
       <div>
-        <h3 className={`${JBMono.className} text-3xl mt-6`}>Where: <a className={"underline hover:text-blue-500"} href={"https://maps.app.goo.gl/8Ti7Rft5gL3n98oc8"}>Coburn Hall</a></h3>
-        <h3 className={`${JBMono.className} text-3xl mt-4`}>When: April 5th, 2025</h3>
+        <h3 className={`${JBMono.className} text-3xl mt-6 text-black`}>Where: <a className={"underline hover:text-blue-500"} href={"https://maps.app.goo.gl/8Ti7Rft5gL3n98oc8"}>Coburn Hall</a></h3>
+        <h3 className={`${JBMono.className} text-3xl mt-4 text-black`}>When: April 5th, 2025</h3>
       </div>
       <ClubCarousel />
-      <h2 id="schedule" className={`${InstrSans.className} text-6xl m-6 font-bold`}>Schedule</h2>
+      <h2 id="schedule" className={`${InstrSans.className} text-6xl m-6 font-bold text-black`}>Schedule</h2>
       <Itinerary />
     </div>
   );
--- src/components/ClubCarouselCard.tsx
+++ src/components/ClubCarouselCard.tsx
@@ -2,7 +2,7 @@ import { JBMono } from "@/fonts"
 import Image from "next/image"
 export const ClubCarouselCard = ({ name, img_url }: { name: string, img_url?: string }) => {
     return (
-        <div className="bg-red-600 flex-none flex flex-col items-center p-4 gap-6 justify-center text-wrap basis-60 grow-1 shadow-xl rounded-2xl shadow-amber-600">
+        <div className="bg-pink-500 flex-none flex flex-col items-center p-4 gap-6 justify-center text-wrap basis-60 grow-1 shadow-xl rounded-2xl shadow-amber-600">
             <h2 className={`${JBMono.className} text-2xl text-center`}>{name}</h2>
             {img_url ? <img alt={name + "Logo"} src={img_url} className={"club_logo"} /> : <Image alt="test" width={75} height={75} src={"/globe.svg"} className={""} />}
         </div>
